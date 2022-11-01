57f4904785e96a54b606e9cb6f31a6f504004d05
Brandon <brandon.thio@yahoo.com.sg>
2022-11-01 16:33:02 +0800

    Change color property name for Project and Tag to avoid name collision

--- Sources/HighlightedTextEditor/System Extensions/TextFormattingRule.swift
+++ Sources/HighlightedTextEditor/System Extensions/TextFormattingRule.swift
@@ -10,13 +10,13 @@ import SwiftUI
 #if os(iOS) && BETA
 public extension TextFormattingRule {
     @available(iOS 14.0, *)
-    init(foregroundColor color: Color, fontTraits: SymbolicTraits = []) {
-        self.init(key: .foregroundColor, value: UIColor(color), fontTraits: fontTraits)
+    init(foregroundColor resolvedColor: Color, fontTraits: SymbolicTraits = []) {
+        self.init(key: .foregroundColor, value: UIColor(resolvedColor), fontTraits: fontTraits)
     }
 
     @available(iOS 14.0, *)
-    init(highlightColor color: Color, fontTraits: SymbolicTraits = []) {
-        self.init(key: .backgroundColor, value: UIColor(color), fontTraits: fontTraits)
+    init(highlightColor resolvedColor: Color, fontTraits: SymbolicTraits = []) {
+        self.init(key: .backgroundColor, value: UIColor(resolvedColor), fontTraits: fontTraits)
     }
 }
 #endif
@@ -24,13 +24,13 @@ public extension TextFormattingRule {
 #if os(macOS) && BETA
 public extension TextFormattingRule {
     @available(macOS 11.0, *)
-    init(foregroundColor color: Color, fontTraits: SymbolicTraits = []) {
-        self.init(key: .foregroundColor, value: NSColor(color), fontTraits: fontTraits)
+    init(foregroundColor resolvedColor: Color, fontTraits: SymbolicTraits = []) {
+        self.init(key: .foregroundColor, value: NSColor(resolvedColor), fontTraits: fontTraits)
     }
 
     @available(macOS 11.0, *)
-    init(highlightColor color: Color, fontTraits: SymbolicTraits = []) {
-        self.init(key: .backgroundColor, value: NSColor(color), fontTraits: fontTraits)
+    init(highlightColor resolvedColor: Color, fontTraits: SymbolicTraits = []) {
+        self.init(key: .backgroundColor, value: NSColor(resolvedColor), fontTraits: fontTraits)
     }
 }
 #endif
